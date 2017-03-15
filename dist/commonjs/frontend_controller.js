636a49abff1763e2da106549f296ab94fe9f73e0
Simon Reymann <simon.reymann@gmail.com>
2017-03-15 19:28:16 +0100

    build dist

--- dist/commonjs/frontend_controller.js
+++ dist/commonjs/frontend_controller.js
@@ -34,6 +34,7 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
         if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
 };
+Object.defineProperty(exports, "__esModule", { value: true });
 var FrontendController = (function () {
     function FrontendController(frontendService) {
         this._frontendService = undefined;
